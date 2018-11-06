83efc21962967d5ad41d2943bf08d0bc79a80e8f
Aaron Teague <intern5@phinternprogram.onmicrosoft.com>
2018-11-05 23:21:25 -0500

    update to folders

---
+++ conversionCalcHW8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  conversionCalcHW8
+//
+//  Created by CIS Student on 11/5/18.
+//  Copyright © 2018 AnthonyAtone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
